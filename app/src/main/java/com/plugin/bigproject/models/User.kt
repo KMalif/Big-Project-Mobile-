8eb7c2956c416f62b81bb19088db8a5b4e2e6384
KMalif <khafidulmualif2019@gmail.com>
2021-11-17 09:20:55 +0700

    1. Initial commit

---
+++ app/src/main/java/com/plugin/bigproject/models/User.kt
@@ -0,0 +1,10 @@
+package com.plugin.bigproject.models
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class User(
+    @SerializedName("id") var id : String? = null
+):Parcelable
