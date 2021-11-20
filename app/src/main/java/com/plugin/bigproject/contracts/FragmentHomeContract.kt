d013e22edd16082c81597b9d0e57eed42b6d6df5
KMalif <khafidulmualif2019@gmail.com>
2021-11-21 03:18:24 +0700

    5. Create list hair cut and update on profile fragment

--- app/src/main/java/com/plugin/bigproject/contracts/FragmentHomeContract.kt
+++ app/src/main/java/com/plugin/bigproject/contracts/FragmentHomeContract.kt
@@ -1,4 +1,16 @@
 package com.plugin.bigproject.contracts
 
 interface FragmentHomeContract {
+    interface FragmentHomeView{
+        fun attachTrendToRecycler()
+        fun attachMitraToRecycler()
+        fun showLoading()
+        fun hideLoading()
+    }
+
+    interface FragmentHomePresenter{
+        fun getTrend()
+        fun getMitra()
+        fun destroy()
+    }
 }
--- app/src/main/java/com/plugin/bigproject/webservices/APIServices.kt
+++ app/src/main/java/com/plugin/bigproject/webservices/APIServices.kt
@@ -1,4 +1,9 @@
 package com.plugin.bigproject.webservices
 
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface APIServices {
+
+
 }
