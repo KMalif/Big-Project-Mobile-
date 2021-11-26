00a341bea671457083ffae41ca24878a7469d458
KMalif <khafidulmualif2019@gmail.com>
2021-11-26 23:29:18 +0700

    7. Create Camera Activity

---
+++ app/src/main/java/com/plugin/bigproject/activities/CameraActivity.kt
@@ -0,0 +1,92 @@
+package com.plugin.bigproject.activities
+
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import com.plugin.bigproject.databinding.ActivityCameraBinding
+
+class CameraActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCameraBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        btnCamera()
+        btnGalery()
+    }
+
+    private fun btnGalery(){
+        binding.BtnChooseImage.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED){
+                    val permissions = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+
+                    requestPermissions(permissions,PERMISSION_CODE)
+                }
+                else{
+                    pickImageFromGallery()
+                }
+            }
+            else{
+                pickImageFromGallery()
+            }
+        }
+    }
+    private fun btnCamera(){
+        binding.BtnCamera.setOnClickListener {
+            val i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(i, 123)
+        }
+    }
+
+    private fun pickImageFromGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICKCODE)
+    }
+
+    companion object {
+        private val IMAGE_PICKCODE = 1000
+        private val PERMISSION_CODE = 1001
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGallery()
+                }
+                else{
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICKCODE){
+            binding.ImageDetail.setImageURI(data?.data)
+        }
+
+        if (requestCode==123)
+        {
+            val bmp= data?.extras?.get("data") as? Bitmap
+            binding.ImageDetail.setImageBitmap(bmp)
+        }
+    }
+}
+
--- app/src/main/java/com/plugin/bigproject/activities/MainActivity.kt
+++ app/src/main/java/com/plugin/bigproject/activities/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.plugin.bigproject.activities
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.github.florent37.runtimepermission.kotlin.askPermission
 import com.plugin.bigproject.R
 import com.plugin.bigproject.databinding.ActivityMainBinding
-import com.plugin.bigproject.fragments.CameraFragment
 import com.plugin.bigproject.fragments.HomeFragment
-import com.plugin.bigproject.fragments.MapsFragment
 import com.plugin.bigproject.fragments.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
@@ -17,19 +19,58 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+        binding.BottomNavigation.background = null
         bottomNavigatio()
+        askPermissions()
+        fab()
         setCurrentFragment(HomeFragment())
+    }
+
+    private fun askPermissions(){
+        askPermission(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.CAMERA
+        ){
+
+        }.onDeclined{e ->
+            if (e.hasDenied()){
+                e.denied.forEach{
+
+                }
+                AlertDialog.Builder(this)
+                    .setMessage("Please Accept Our Permission")
+                    .setPositiveButton("Yes"){_,_ ->
+                        e.askAgain()
+                    }
+                    .setNegativeButton("No"){dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
+            }
+
+            if (e.hasForeverDenied()){
+                e.foreverDenied.forEach {
+                }
+                e.goToSettings()
+            }
+        }
     }
 
     private fun bottomNavigatio(){
         binding.BottomNavigation.setOnNavigationItemSelectedListener {
             when(it.itemId) {
                 R.id.icHome -> setCurrentFragment(HomeFragment())
-                R.id.icCamera -> setCurrentFragment(CameraFragment())
-                R.id.icMaps -> setCurrentFragment(MapsFragment())
+//                R.id.icMaps -> setCurrentFragment(MapsFragment())
                 R.id.icProfile->setCurrentFragment(ProfileFragment())
             }
             true
+        }
+    }
+
+    private fun fab(){
+        binding.fab.setOnClickListener {
+            startActivity(Intent(this, CameraActivity::class.java))
         }
     }
 
@@ -39,5 +80,7 @@ class MainActivity : AppCompatActivity() {
             commit()
         }
     }
+
+
 
 }
